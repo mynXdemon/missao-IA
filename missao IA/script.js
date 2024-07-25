c36b93df409312c99cbbb5314e53567011925ab7
mynXdemon <129210494+mynXdemon@users.noreply.github.com>
2024-07-25 09:48:53 -0300

    Add files via upload
    fiz aula 3 apenas o java

--- missao IA/script.js
+++ missao IA/script.js
@@ -2,3 +2,38 @@ const caixaPrincpal = document.querySelector(".caixa-principal");
 const caixaPerguntas  = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resulatdo");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: 
+    }
+]
